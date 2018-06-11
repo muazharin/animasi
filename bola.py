e98d1e14a43e86caa67622e45e7cabd5b14eac23
muazharin <alfanmuazharin@gmail.com>
2018-06-11 10:24:19 +0800

    bola final

--- bola.py
+++ bola.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from Tkinter import *
 import time
 
 lebar = 600
@@ -11,12 +11,31 @@ tk.title("Gambar Bola Bergerak")
 canvas.pack()
 
 bola = canvas.create_oval(10, 10, 70, 70, fill="orange")
+bola2 = canvas.create_oval(10, 10, 70, 70, fill="blue")
+bola3 = canvas.create_oval(10, 10, 70, 70, fill="red")
 kecepatanx = 4
 kecepatany = 5
-
+kecepatanx1 = 2
+kecepatany1 = 3
+kecepatanx2 = 3
+kecepatany2 = 4
 while True:
     canvas.move(bola, kecepatanx, kecepatany)
+    canvas.move(bola2, kecepatanx1, kecepatany1)
+    canvas.move(bola3, kecepatanx2, kecepatany2)
     pos = canvas.coords(bola)
+    pos2 = canvas.coords(bola2)
+    pos3 = canvas.coords(bola3)
+    if pos3[3] >= tinggi or pos3[1] <= 0:
+        kecepatany2 = -kecepatany2
+    if pos3[2] >= lebar or pos3[0] <= 0:
+        kecepatanx2 = -kecepatanx2
+    
+    if pos2[3] >= tinggi or pos2[1] <= 0:
+        kecepatany1 = -kecepatany1
+    if pos2[2] >= lebar or pos2[0] <= 0:
+        kecepatanx1 = -kecepatanx1
+    
     if pos[3] >= tinggi or pos[1] <= 0:
         kecepatany = -kecepatany
     if pos[2] >= lebar or pos[0] <= 0:
