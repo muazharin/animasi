5c02662ee11e4dba2402f8b33a58a9173618d02d
muazharin <alfanmuazharin@gmail.com>
2018-06-14 00:34:25 +0800

    Laporan Final Animasi

--- bola.py
+++ bola.py
@@ -9,16 +9,20 @@ canvas = Canvas(tk, width=lebar, height=tinggi)
 tk.title("Gambar Bola Bergerak")
 
 canvas.pack()
-
+#membuat bola
 bola = canvas.create_oval(10, 10, 70, 70, fill="orange")
 bola2 = canvas.create_oval(10, 10, 70, 70, fill="blue")
 bola3 = canvas.create_oval(10, 10, 70, 70, fill="red")
+
+#mengatur kecepatan bola
 kecepatanx = 4
 kecepatany = 5
 kecepatanx1 = 2
 kecepatany1 = 3
 kecepatanx2 = 3
 kecepatany2 = 4
+
+#melooping bola dengan menggunakan kecepatan x dan y
 while True:
     canvas.move(bola, kecepatanx, kecepatany)
     canvas.move(bola2, kecepatanx1, kecepatany1)
